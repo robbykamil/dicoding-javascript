cf57d0a28d23a437de0992d6dc92fb0816342450
Robby Kamil <71558401+robbykamil@users.noreply.github.com>
2022-11-21 05:44:20 +0700

    route configuration

---
+++ be/hapi/routes.js
@@ -0,0 +1,63 @@
+const routes = [
+  {
+    method: '*',
+    path: '/',
+    handler: (req, h) => {
+      return 'Hompeage cannot be access with this method';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => {
+      return 'Homepage';
+    },
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: (req, h) => {
+      const { username, password } = req.payload;
+      console.log(password);
+      return `Welcome, ${username}`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/user/{username?}',
+    handler: (req, h) => {
+      //path parameter, with 'stranger' as default of parameter value
+      const { username = 'stranger'} = req.params;
+      
+      //query parameter
+      const { lang } = req.query;
+      
+      if(lang === 'id') {
+        return `Halo, ${username}! Selamat Datang di Aplikasi ini`;
+      }
+      
+      return `Hi, ${username}! Welcome to this app`;
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (req, h) => {
+      return 'About page cannot be access with this method';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (req, h) => {
+      return `About Page`;
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (req, h) => {
+      return 'Page not found';
+    },
+  },
+];
